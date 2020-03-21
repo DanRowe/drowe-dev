28ccd85abb64d28c4157c5ca77722990c052b5ff
Daniel Rowe <drowe15@montgomerycollege.edu>
2020-03-21 16:01:29 -0400

    Added bootstrap link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import React from "react"
+// TODO customize bootstrap to match what was used --> https://getbootstrap.com/docs/4.3/getting-started/theming/
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from "gatsby"
 
 import Layout from "../components/Layout/layout"
