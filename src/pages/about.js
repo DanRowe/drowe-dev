18bac31574a05074fc32267eb232257026cbdea7
DanRowe1 <drowe98@live.com>
2020-04-30 18:59:58 -0400

    added basic about page content

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,1 +1,16 @@
 // TODO about page
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/Layout/layout"
+import SEO from "../components/seo"
+
+const AboutPage = () => (
+  <Layout>
+    <SEO title="About Page" />
+    <h1>Hi from the second page</h1>
+    <p>Welcome to page 2</p>
+  </Layout>
+)
+
+export default AboutPage
