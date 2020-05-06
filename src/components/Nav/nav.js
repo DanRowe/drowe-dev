719e552971acceca13ec1d6133210c9de08d1bbd
DanRowe1 <drowe98@live.com>
2020-05-06 01:41:25 -0400

    text formatting

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -2,7 +2,7 @@ import React from "react"
 import { NavColumn } from './nav-column'
 import { Link } from "gatsby";
 
-export default({page}) => (
+export default({ page }) => (
   <>
     {page.map(link => (
             <Link to={link.link} style={{textDecoration: 'none', color: 'black'}}><NavColumn>{link.name}</NavColumn></Link>
