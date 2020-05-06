2ac19da2cc26d3d5ebbb5658ef4d1a98801b11ca
DanRowe1 <drowe98@live.com>
2020-05-06 00:36:53 -0400

    moved navigation logic to layout

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -11,18 +11,48 @@ import React from "react"
 import Footer from "../Footer/footer"
 import Nav from "../Nav/nav"
 import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import { StaticQuery, graphql } from "gatsby";
 import Row from 'react-bootstrap/Row'
+// import styled from 'styled-components'
 
+// const ContentContainer = styled.div`
+//   max-width: 1300px;
+//   padding: 1rem;
+//   margin: auto;
+// `
 
 export default ({children, page}) => (
-  <Container fluid>
-    <Row>
-      <Nav page={page}>
-        {children}
-      </Nav>
-    </Row>
-    <Row className="fixed-bottom">
-      <Footer />
-    </Row>
-  </Container>
+  <StaticQuery
+    query={graphql`
+      query SiteQuery {
+        site {
+          siteMetadata {
+            menuLinks {
+              name
+              link
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Container fluid>
+        <Row>
+          <Col className="p-0 d-inline-flex">
+            <Nav page={data.site.siteMetadata.menuLinks.slice(0,data.site.siteMetadata.menuLinks.map(e => e.name).indexOf(page)+1)}/>
+          </Col>
+          <Col xs={10} className="p-0">
+            {children}
+          </Col>
+          <Col className="p-0 d-none d-lg-block">
+            <Nav page={data.site.siteMetadata.menuLinks.slice(data.site.siteMetadata.menuLinks.map(e => e.name).indexOf(page)+1,)}/>
+          </Col>
+        </Row>
+        <Row className="fixed-bottom">
+          <Footer />
+        </Row>
+      </Container>
+    )}
+    />
 )
--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -1,51 +1,11 @@
 import React from "react"
-import styled from 'styled-components';
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 import { NavColumn } from './nav-column'
-import { StaticQuery, graphql, Link } from "gatsby";
+import { Link } from "gatsby";
 
-export default({children, page}) => (
-  <StaticQuery
-    query={graphql`
-      query SiteQuery {
-        site {
-          siteMetadata {
-            menuLinks {
-              name
-              link
-            }
-          }
-        }
-      }
-    `}
-    render={data => (
-      <Container fluid className="no-gutters">
-      <Row>
-        {/* active page and before */}
-        <Col className="p-0 d-inline-flex">
-        {data.site.siteMetadata.menuLinks.slice(0,data.site.siteMetadata.menuLinks.map(e => e.name).indexOf(page)+1).map(link => (
-                <Link to={link.link} style={{textDecoration: 'none', color: 'black'}}><NavColumn>{link.name}</NavColumn></Link>
-              ))}
-        </Col>
-        {/* page content */}
-        <Col xs={10} className="p-0">{ children }</Col>
-        {/* pages after active*/}
-        <Col className="p-0 d-none d-lg-block">
-          <Container className="no-gutters">
-            <Row>
-              <Col></Col>
-              <Col xs={7} className="p-0 d-inline-flex">
-              {data.site.siteMetadata.menuLinks.slice(data.site.siteMetadata.menuLinks.map(e => e.name).indexOf(page)+1,).map(link => (
-                <Link to={link.link} style={{textDecoration: 'none', color: 'black'}}><NavColumn>{link.name}</NavColumn></Link>
-              ))}
-              </Col>
-            </Row>
-          </Container>
-        </Col>
-      </Row>
-    </Container>
-    )}
-  />
+export default({page}) => (
+  <>
+    {page.map(link => (
+            <Link to={link.link} style={{textDecoration: 'none', color: 'black'}}><NavColumn>{link.name}</NavColumn></Link>
+    ))}
+  </>
 )
