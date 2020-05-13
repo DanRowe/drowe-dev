04288cfc92b4be9521713f0db2ecf89c5c951850
DanRowe1 <drowe98@live.com>
2020-05-13 19:44:36 -0400

    Turned title into a card

--- src/pages/development.js
+++ src/pages/development.js
@@ -1,23 +1,33 @@
 import React from "react"
 
 import SEO from "../components/seo"
-import {Row, Col, Container} from "react-bootstrap"
+import {Row, Col, Container, Card} from "react-bootstrap"
 
 
 const DevelopmentPage = () => (
-    <Row className="h-100">
+    <Row className="h-100 align-items-center">
         <SEO title="Under Construction" />
-        <Col className="my-auto">
-            <div>
-                <h1 className="d-none d-sm-block">Nice to meet you</h1>
-                <h2 className="d-sm-none">Nice to meet you</h2>
-                <p>More amazing and exciting content to come.</p>
-                <p>For now, take a look at my LinkedIn profile.</p>
-            </div>
+        <Col className="m-auto">
+            <Card
+                bg="light"
+                style={{width: '30rem', margin: "auto"}}
+            >
+                <Card.Body>
+                    <Card.Title>Nice to meet you</Card.Title>
+                    <Card.Text>
+                        More amazing and exciting content to come.
+                    </Card.Text>
+                    <Card.Text>
+                        For now, take a look at my LinkedIn profile.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </Col>
+        <Col>
             <div>
                 <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
                 <div 
-                    class="LI-profile-badge"  
+                    className="LI-profile-badge"  
                     data-version="v1" 
                     data-size="large" 
                     data-locale="en_US" 
@@ -26,7 +36,7 @@ const DevelopmentPage = () => (
                     data-vanity="danielrowe2"
                     >
                     <a 
-                        class="LI-simple-link" 
+                        className="LI-simple-link" 
                         href='https://www.linkedin.com/in/danielrowe2?trk=profile-badge'
                         >
                         Daniel Rowe's LinkedIn Profile
