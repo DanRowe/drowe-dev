015cff641033c8b97db9bca4a24ba349a223019f
DanRowe1 <drowe98@live.com>
2020-05-13 22:18:18 -0400

    styled paragraph text in a card

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container'
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 
 import Layout from "../components/Layout/layout"
 import SEO from "../components/seo"
@@ -18,18 +19,24 @@ const IndexPage = () => (
         <Col className="d-none d-md-block" style={{maxHeight: "91vh"}}><Headshot></Headshot></Col>
         <Col className="d-none d-sm-block d-md-none" style={{maxHeight: "60vh"}}><Headshot></Headshot></Col>
         <Col className="d-sm-none" style={{maxHeight: "40vh"}}><Headshot></Headshot></Col>
+        {/* <Col style={{maxHeight: "auto"}}><Headshot></Headshot></Col> */}
         <Col className="d-flex align-items-center">
           <div>
             <h1>Hi I&rsquo;m Daniel!</h1>
-            <p style={{textAlign: "left"}}>
-              This is where I will eventually write a nice paragraph of text 
-              about myself. It will explain my intetests, why someone should 
-              hire me, who I am, etc. This is a realistic amount of space 
-              that the text blurb will take up. Before, my example was 
-              far too long to be considered a realistic size. Now I can 
-              properly style my page to match the amount of space that 
-              this will take up.
-            </p>
+            <Card
+              bg="light"
+              border="primary"
+              body
+              style={{textAlign: "left"}}
+            >
+                  This is where I will eventually write a nice paragraph of text 
+                  about myself. It will explain my intetests, why someone should 
+                  hire me, who I am, etc. This is a realistic amount of space 
+                  that the text blurb will take up. Before, my example was 
+                  far too long to be considered a realistic size. Now I can 
+                  properly style my page to match the amount of space that 
+                  this will take up.
+            </Card>
           </div>
         </Col>
       </Row>
