38b8918a9c40c856177a16af8208bd20fe72f8f9
DanRowe1 <drowe98@live.com>
2020-05-14 19:18:46 -0400

    rounded and thinner

--- src/components/Nav/nav-column.js
+++ src/components/Nav/nav-column.js
@@ -3,12 +3,13 @@ import styled from 'styled-components';
 export const NavColumn = styled.div `
 // FIXME nav columns don't display on Edge
 height: calc(100vh - 2.2rem);
-// max-width: 2.2em;
-width: 3.5em;
-border: 1px solid black;
+width: 3em;
+border: 1px solid gray;
+border-width: 1px !important;
+border-radius: 8px;
 writing-mode: vertical-rl;
 text-align: center;
 text-orientation: upright;
 overflow: hidden;
-padding-right: 1em;
+padding-right: .65em;
 `;
