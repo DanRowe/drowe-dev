4236ec5e10b2e14598886a676c1bc1619cc3fc61
DanRowe1 <drowe98@live.com>
2020-05-14 20:13:32 -0400

    active styling using Link

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -5,11 +5,20 @@ import { Link } from "gatsby";
 export default({ page }) => (
   <>
     {page.map(link => (
-            <Link 
-              to={link.link} 
-              style={{textDecoration: 'none'}}
-              key={link.name}
-            ><NavColumn className="btn-outline-primary">{link.name}</NavColumn></Link>
+      <Link 
+        to={link.link} 
+        style={{textDecoration: 'none'}}
+        key={link.name}
+        activeClassName="active"
+        className="btn-outline-primary"
+        style={{
+          borderWidth: "1px !important", 
+          borderRadius: "8px",
+          textDecoration: "none"
+        }}
+      ><NavColumn 
+        // className="btn-outline-primary"
+      >{link.name}</NavColumn></Link>
     ))}
   </>
 )
