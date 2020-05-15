e6a1e1015954ce38d7437b042f59f3f251bc2106
DanRowe1 <drowe98@live.com>
2020-05-14 21:19:20 -0400

    proper active styling

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -2,23 +2,29 @@ import React from "react"
 import { NavColumn } from './nav-column'
 import { Link } from "gatsby";
 
-export default({ page }) => (
-  <>
-    {page.map(link => (
-      <Link 
-        to={link.link} 
-        style={{textDecoration: 'none'}}
-        key={link.name}
-        activeClassName="active"
-        activeStyle={{
-          borderWidth: "1px", 
-          borderRadius: "8px",
-          textDecoration: "none",
-        }}
-        className="btn-outline-primary"
-      ><NavColumn 
-        // className="btn-outline-primary"
-      >{link.name}</NavColumn></Link>
-    ))}
-  </>
-)
+const Navigation = ({ page }) => {
+  const isActive = (link) => {
+    return location.pathname === link
+  }
+
+  return (
+    <>
+      {page.map(link => (
+        <Link 
+          to={link.link} 
+          style={{textDecoration: 'none'}}
+          key={link.name}
+        >
+          <NavColumn 
+          // className="btn-outline-primary"
+          className={isActive(link.link) ? "btn-outline-primary active" : "btn-outline-primary"}
+          >
+            {link.name}
+          </NavColumn>
+        </Link>
+      ))}
+    </>
+  )
+}
+
+export default Navigation
