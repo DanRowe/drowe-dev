f8ce4512c3017726f671ec16faa689254d0198f7
DanRowe1 <drowe98@live.com>
2020-05-23 02:55:16 -0400

    Added submodule for hostinger deployment

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,24 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require("path")
+const fs = require("fs")
+
+exports.onPreInit = () => {
+  if (process.argv[2] === "build") {
+    fs.rmdirSync(path.join(__dirname, "dist"), { recursive: true })
+    fs.renameSync(
+      path.join(__dirname, "public"),
+      path.join(__dirname, "public_dev")
+    )
+  }
+}
+
+exports.onPostBuild = () => {
+  fs.renameSync(path.join(__dirname, "public"), path.join(__dirname, "drowe-dev-live"))
+  fs.renameSync(
+    path.join(__dirname, "public_dev"),
+    path.join(__dirname, "public")
+  )
+}
