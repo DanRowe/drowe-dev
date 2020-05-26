44dd9d39dfcf2e8ca141de76770b146e46f07016
DanRowe1 <drowe98@live.com>
2020-05-26 19:09:34 -0400

    preserve .git in new build folder

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,11 +9,20 @@
 const path = require("path")
 const fs = require("fs")
 
+// REF https://github.com/gatsbyjs/gatsby/issues/14703
+
+// workflow -- 
+// 1. gatsby build
+// 2. create local branch
+// 3. commit changes
+// 4. publish branch
+// 5. merge to master
 exports.onPreInit = () => {
   if (process.argv[2] === "build") {
+    if(!fs.existsSync(path.join(__dirname, "public"))) fs.mkdirSync(path.join(__dirname, "public"))
     fs.renameSync(
-      path.join(__dirname, "drowe-dev-live/.git"),
-      path.join(__dirname, "public/.git")
+      path.join(__dirname, "drowe-dev-live", ".git"),
+      path.join(__dirname, "public", ".git")
     )
     fs.rmdirSync(path.join(__dirname, "drowe-dev-live"), { recursive: true })
   }
